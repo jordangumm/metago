66c8eefbb06693e11c5205864871babf2e47bd76
Jordan Gumm <jngumm@gmail.com>
2018-08-31 15:24:13 -0400

    fixed binning script

--- metago/binning.py
+++ metago/binning.py
@@ -13,10 +13,11 @@ class SampleBinning(FluxWorkflowRunner):
 
     https://github.com/voutcn/megahit/wiki/Assembly-Tips
     """
-    def __init__(self, contig_fp, fastq_fp, output_dp, max_ppn, max_mem):
+    def __init__(self, sid, contig_fp, fastq_fp, min_contig_length, output_dp, max_ppn, max_mem):
         self.contig_fp = contig_fp
         self.fastq_fp = fastq_fp
-        self.output_dp = os.path.join(output_dp, 'binning')
+        self.min_contig_length = min_contig_length
+        self.output_dp = os.path.join(output_dp, sid)
 
         self.max_ppn = int(max_ppn)
         self.max_mem = int(max_mem)
@@ -30,16 +31,10 @@ class SampleBinning(FluxWorkflowRunner):
         fp = os.path.dirname(os.path.abspath(__file__))
         conda = os.path.join(fp, '../dependencies/miniconda/bin/activate')
 
-        sample_output_dp = os.path.join(self.output_dp, self.sid)
-        if not os.path.exists(sample_output_dp): os.makedirs(sample_output_dp)
-
-        if not os.path.exists(assembly_dp):
-            os.makedirs(assembly_dp)
-        if len(os.listdir(assembly_dp)) == 0:
-            cmd = 'source {} && run_MaxBin.pl -contig {} -reads {} -thread {} -out {}'.format(
-                             conda, self.contig_fp, self.fastq_fp, self.max_ppn, sample_output_dp)
-            print 'cmd: {}'.format(cmd)
-            self.addTask('bin', nCores=self.max_ppn, memMb=self.max_mem, command=cmd)
+        cmd = 'source {} && run_MaxBin.pl -contig {} -reads {} -thread {} -out {} -min_contig_length {}'.format(
+                         conda, self.contig_fp, self.fastq_fp, self.max_ppn, self.output_dp, self.min_contig_length)
+        print 'cmd: {}'.format(cmd)
+        self.addTask('bin', nCores=self.max_ppn, memMb=self.max_mem, command=cmd)
 
 
 @click.group()
@@ -57,20 +52,16 @@ def cli(ctx, output, ppn, mem):
 @cli.command()
 @click.option('--contig_fp', '-c', required=True, help='file path to contig fasta file')
 @click.option('--read_fp', '-r', required=True, help='file path to read fasta/fastq file')
+@click.option('--min_contig_length', '-m', default=1500)
 @click.pass_context
-def sample_binning(ctx, sample_dp):
+def sample_binning(ctx, contig_fp, read_fp, min_contig_length):
+    sid = read_fp.split('/')[-1].split('.')[0]
+
     r = ''.join(random.choice(string.ascii_uppercase + string.digits) for _ in range(8))
-    log_output_dp = os.path.join(ctx.obj['OUTPUT'], 'logs/sample_assembly_{}'.format(r))
+    log_output_dp = os.path.join(ctx.obj['OUTPUT'], sid, 'logs/sample_assembly_{}'.format(r))
 
-    sample_fastq = ''
-    for fastq in os.listdir(sample_dp):
-        if 'fastq' not in fastq: continue
-        fastq_fp = os.path.join(sample_dp, fastq)
-        sample_fastq = fastq_fp
-        break # this file should be a single interleaved and quality controlled fastq
-
-    runner = SampleBinning(contig_fp=contig_fp, fastq_fp=read_fp, output_dp=ctx.obj['OUTPUT'],
-                                                max_ppn=ctx.obj['PPN'], max_mem=ctx.obj['MEM'])
+    runner = SampleBinning(sid=sid, contig_fp=contig_fp, fastq_fp=read_fp, min_contig_length=min_contig_length,
+                           output_dp=ctx.obj['OUTPUT'], max_ppn=ctx.obj['PPN'], max_mem=ctx.obj['MEM'])
     runner.run(mode='local', dataDirRoot=log_output_dp, nCores=ctx.obj['PPN'], memMb=ctx.obj['MEM']) 
 
 
